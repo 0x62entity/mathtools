3bba13e03df9ea60606c5a1da29eebb763dd4a6f
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-01 13:52:48 +1100

    create some pages

---
+++ app/tools/calculator/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Calculator() {
+  return (
+    <div className="flex flex-col min-h-screen bg-zinc-50 font-sans dark:bg-black p-4">
+      <header className="flex flex-row justify-start items-center">
+        <Link href="/" className="m-2 font-semibold">MathTools</Link>
+        <h1 className="font-bold text-2xl m-2">Calculator</h1>
+      </header>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
