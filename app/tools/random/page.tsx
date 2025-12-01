8eca98db875ef88e05725c76182874393f0972bf
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-01 14:36:19 +1100

    random generator

--- app/tools/random/page.tsx
+++ app/tools/random/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Random() {
+  const [num, setNum] = useState<number>(0);
+  const [min, setMin] = useState<number>(0);
+  const [max, setMax] = useState<number>(0);
   return (
     <div className="flex flex-col min-h-screen bg-zinc-50 font-sans dark:bg-black p-4">
       <header className="flex flex-row justify-start items-center">
@@ -8,7 +14,13 @@ export default function Random() {
         <h1 className="font-bold text-2xl m-2">Random Generator</h1>
       </header>
       <div>
-        
+        <input type="number" placeholder="Minimum" onChange={(e) => setMin(Number.parseInt(e.target.value))} className="bg-zinc-900 p-2 m-1 rounded-md"/>
+        <br/>
+        <input type="number" placeholder="Maximum" onChange={(e) => setMax(Number.parseInt(e.target.value))} className="bg-zinc-900 p-2 m-1 rounded-md"/>
+        <br/>
+        <button onClick={() => setNum(Math.floor(Math.random() * (max - min)) + min)} className="bg-blue-600 p-2 m-1 rounded-md">Generate</button>
+        <br/>
+        <label>{num}</label>
       </div>
     </div>
   )
