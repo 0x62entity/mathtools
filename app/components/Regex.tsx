3d0281d2737cc8253ab89d4b86cb73d55d4bc179
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-04 14:35:23 +1100

    begin regex ui

--- app/components/Regex.tsx
+++ app/components/Regex.tsx
@@ -1,9 +1,23 @@
+"use client";
 
+import { useEffect, useState } from "react";
 
 export default function Regex() {
+  const [input, setInput] = useState<string>("");
+  const [tests, setTests] = useState<string>("");
+  const [output, setOutput] = useState<string>("");
+
+  useEffect(() => {
+    const re = new RegExp(input);
+  }, [input]);
+
   return (
     <div>
-
+      <input className="bg-zinc-900 p-2 m-1 rounded-md w-full" placeholder="RegEx..." onChange={(e) => setInput(e.target.value)}/>
+      <div>
+        {output}
+      </div>
+      <textarea className="bg-zinc-900 p-2 m-1 w-full h-64" placeholder="Test Matches (newline seperated)" onChange={(e) => setTests(e.target.value)}/>
     </div>
   )
 }
