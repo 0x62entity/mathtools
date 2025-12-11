8472571271d4b65c0d32a875e6bb8ebee5cad2a3
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-11 14:17:34 +1100

    finish regex ui

--- app/components/Regex.tsx
+++ app/components/Regex.tsx
@@ -12,12 +12,17 @@ export default function Regex() {
   }, [input]);
 
   return (
-    <div>
-      <input className="bg-zinc-900 p-2 m-1 rounded-md w-full" placeholder="RegEx..." onChange={(e) => setInput(e.target.value)}/>
-      <div>
-        {output}
+    <div className="flex flex-row">
+      <div className="w-full m-4">
+        <input className="bg-zinc-900 p-2 m-1 rounded-md w-full" placeholder="RegEx..." onChange={(e) => setInput(e.target.value)}/>
+        <textarea className="bg-zinc-900 p-2 m-1 w-full h-64" placeholder="Test strings (newline seperated)" onChange={(e) => setTests(e.target.value)}/>
       </div>
-      <textarea className="bg-zinc-900 p-2 m-1 w-full h-64" placeholder="Test Matches (newline seperated)" onChange={(e) => setTests(e.target.value)}/>
+      <div className="m-4 w-[30%]">
+        <h3 className="text-xl font-semibold">Matches</h3>
+        <div>
+          {output}
+        </div>
+      </div>
     </div>
   )
 }
