ee6fc35e7d9c02beb753a5e83533cce2a5758144
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-14 10:10:46 +1100

    2048 create random board

--- app/0x67616d6573/2048/page.tsx
+++ app/0x67616d6573/2048/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import { Titillium_Web } from "next/font/google";
+import { useEffect, useState } from "react";
 
 function Cell({ score }: { score: number }) {
   return (
@@ -11,24 +12,50 @@ function Cell({ score }: { score: number }) {
 }
 
 export default function G2048() {
-  const [values, setValues] = useState([
-    [0, 0, 0, 0],
-    [0, 0, 2, 0],
-    [0, 0, 0, 0],
-    [0, 0, 2, 0]
-  ]);
-  
+  const [board, setBoard] = useState<number[][]>([]);
+
+  function tile(board: number[][]) {
+    let empty: [number, number][] = [];
+    board.forEach((r, i) => {
+      r.forEach((c, j) => {
+        if (c == 0) empty.push([i, j]);
+      });
+    });
+
+    if (empty.length == 0) return board;
+
+    const [r, c] = empty[Math.floor(Math.random() * empty.length)];
+    const board1 = board.map(r => [...r]);
+    board1[r][c] = 2;
+    return board1;
+  }
+
+  function reset() {
+    const board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    setBoard(tile(tile(board)));
+  }
+
+  useEffect(() => {
+    reset();
+  }, [])
+
   return (
     <div className="w-108 h-108 flex flex-col items-center justify-center rounded-md bg-zinc-900">
       <div>
-        {values.map((row, rowIndex) => (
-          <div key={rowIndex} className="flex flex-row w-96 h-24 m-2">
-            {row.map((value, colIndex) => (
-              <Cell key={colIndex} score={value} />
+        {board.map((row, i) => (
+          <div key={i} className="flex flex-row w-96 h-24 m-2">
+            {row.map((value, j) => (
+              <Cell key={j} score={value} />
             ))}
           </div>
         ))}
       </div>
+      <span>Arrow keys to play</span>
     </div>
   );
 }
